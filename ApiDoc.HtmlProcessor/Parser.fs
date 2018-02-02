a9854710d83b22862feb9bd4b3ce3036239b5317
Sergey Yacovlev <sergio.yacovlev@outlook.com>
2018-02-03 00:12:52 +0200

    Lazy binding: no evaluation takes place until parcer is given input.

--- ApiDoc.HtmlProcessor/Parser.fs
+++ ApiDoc.HtmlProcessor/Parser.fs
@@ -2,21 +2,30 @@
 
 type Parser<'Result> = ParserImpl of (char list -> ('Result option * char list))
 
-let return_parser initial = (fun symbols -> (Some(initial), symbols)) |> ParserImpl
-let broken_parser<'a> = (fun symbols -> (Option<'a>.None, symbols)) |> ParserImpl
+let return_parser initial = 
+    (fun symbols -> (Some(initial), symbols)) 
+    |> ParserImpl
 
-let run_parser parser = 
+let private get_parser_impl parser = 
     match parser with
     | ParserImpl parserImpl -> parserImpl
-let (-->) parser symbols = (run_parser parser) symbols
+let (-->) symbols parser = (get_parser_impl parser) symbols
 
 let bind (f:('a -> Parser<'b>)) parser = 
-    run_parser parser
-    >> (fun (result, symbols') -> if result |> Option.isSome 
-                                  then f (Option.get result) --> symbols'
-                                  else (None, symbols'))
+    get_parser_impl parser
+    |> (fun parserImpl -> parserImpl >> (fun (result, symbols') -> if result |> Option.isSome 
+                                                                   then symbols' --> f (Option.get result)
+                                                                   else (None, symbols')))
     |> ParserImpl
-let (>>=) parser f  = bind f parser
+let (>>=) parser f = bind f parser
+let (>>==) lparser rparser = lparser >>= (fun _ -> rparser)
+
+let next_char_parser = 
+    (fun symbols -> 
+        match symbols with
+        | [] -> (None, [])
+        | h::t -> (Some(h), t))
+    |> ParserImpl
 
 
 
--- ApiDoc.HtmlProcessor/Program.fs
+++ ApiDoc.HtmlProcessor/Program.fs
@@ -2,9 +2,19 @@
 // See the 'F# Tutorial' project for more help.
 
 open System
+open Parser
+
+
 
 [<EntryPoint>]
 let main argv =        
+    
+    ['a'..'z'] -->
+    (next_char_parser
+    >>== next_char_parser
+    >>== next_char_parser
+    >>== next_char_parser)
+    |> (printfn "%A")
 
     Console.ReadLine() |> ignore
 
